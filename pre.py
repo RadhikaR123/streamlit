bb062727e2257a0a0b8ce4a33ea7a3b612bf43b1
Your Name <you@example.com>
2022-07-11 10:44:32 +0530

    some changes in weather.py

--- pre.py
+++ pre.py
@@ -2,21 +2,33 @@ import csv
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import date, datetime
 
+c_date=(date.today())
+print(type(c_date))
 
-data = pd.read_csv("pre_hist.csv")
-df1 = pd.DataFrame(data)
-# print(df1)
-# for row in range(3):
-#     print(df1[row])
+with open("pune_weather.csv","r") as f:
+    file = csv.DictReader(f)
+    for row in file:
+        if (dict(row)["datetime"]) == str(c_date):
+            print("yes")
+        else:
+            pass
+        
 
-# print(data["num1"])
-# st.dataframe(df1[["num1","num2"]])
+# data = pd.read_csv("pre_hist.csv")
+# df1 = pd.DataFrame(data)
+# # print(df1)
+# # for row in range(3):
+# #     print(df1[row])
 
-fig = plt.figure(figsize=(10,5))
+# # print(data["num1"])
+# # st.dataframe(df1[["num1","num2"]])
 
-plt.bar(data['num1'],data['num2'])
-plt.xlabel("fgghj")
-plt.ylabel('khug')
-plt.title("radhika")
-st.pyplot(fig)
+# fig = plt.figure(figsize=(10,5))
+
+# plt.bar(data['num1'],data['num2'])
+# plt.xlabel("fgghj")
+# plt.ylabel('khug')
+# plt.title("radhika")
+# st.pyplot(fig)
--- weather.py
+++ weather.py
@@ -4,8 +4,9 @@ import pandas as pd
 import streamlit as st
 import csv
 import matplotlib.pyplot as plt
+from datetime import date
 
-
+current_date = date.today()
 with st.sidebar:
     st.header("weather update")
     choice = st.selectbox("select location",("Pune","Delhi","Banglore"))
@@ -15,10 +16,11 @@ if choice== "Pune":
     st.write("current weather report")
     col1, col2, col3 = st.columns(3)
 
+
     with open("pune_weather.csv","r") as f:
         file = csv.DictReader(f)
         for row in file:
-            if dict(row)["datetime"] == "2022-07-09":
+            if dict(row)["datetime"] == str(current_date):
                 col1.metric("Temperature", dict(row)["temp"])
                 col2.metric("Wind-speed", dict(row)["humidity"] )
                 col3.metric("Humidity", dict(row)["windspeed"])
@@ -35,8 +37,18 @@ if choice== "Pune":
         plt.ylabel('Temprature per day', fontdict={'fontsize': 35, 'fontweight': 'medium'})
         plt.title("temprature per day graph", fontdict={'fontsize': 40, 'fontweight': 'medium'})
         st.pyplot(fig)
+    elif month == "july":
+        data = pd.read_csv("pune_weather.csv", skiprows=range(1,31), nrows=22)
+        df1 = pd.DataFrame(data)
+        st.dataframe(df1[["datetime", "temp", "humidity", "dew", "snow", "windspeed"]])
+        # chart = data.plot(kind = 'scatter', x= 'datetime', y= 'temp')
+        fig = plt.figure(figsize=(30,20))
 
-
+        plt.plot(data['datetime'],data['temp'])
+        plt.xlabel("Date of the month", fontdict={'fontsize': 35, 'fontweight': 'medium'})
+        plt.ylabel('Temprature per day', fontdict={'fontsize': 35, 'fontweight': 'medium'})
+        plt.title("temprature per day graph", fontdict={'fontsize': 40, 'fontweight': 'medium'})
+        st.pyplot(fig)
 
 
 
